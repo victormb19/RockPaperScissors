925ea392f7b7b64fa5e6dff197d93a4ef963117d
Victor <victormb19@gmail.com>
2022-11-03 00:51:33 +0100

    Extract player class

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    public class Player
+    {
+        public int wins;      // # of wins
+        Random random = new Random(DateTime.Now.Millisecond);
+
+        /**
+         * Randomly choose rock, paper, or scissors
+         */
+        public string PlayerChoice()
+        {
+            string choice = "";
+            int c = random.Next(0,3);
+            switch (c)
+            {
+                case 0:
+                    choice = ("rock");
+                    break;
+                case 1:
+                    choice = ("paper");
+                    break;
+                case 2:
+                    choice = ("scissors");
+                    break;
+            }
+            return choice;
+        }
+
+        public int SetWins()
+        {
+            int winTotal = wins++;
+            return winTotal;
+        }
+
+        public int GetWins()
+        {
+            return (wins);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using RockPaperScissors.Utils;
+﻿using RockPaperScissors.Utils;
 
 namespace RockPaperScissors
 {
@@ -8,46 +7,6 @@ namespace RockPaperScissors
         public static void Main(string[] args)
         {
             new RockPaperScissor(new ConsoleOuput()).Play();
-        }
-    }
-
-    public class Player
-    {
-
-        public int wins;      // # of wins
-        Random random = new Random(DateTime.Now.Millisecond);
-
-        /**
-         * Randomly choose rock, paper, or scissors
-         */
-        public string PlayerChoice()
-        {
-            string choice = "";
-            int c = random.Next(0,3);
-            switch (c)
-            {
-                case 0:
-                    choice = ("rock");
-                    break;
-                case 1:
-                    choice = ("paper");
-                    break;
-                case 2:
-                    choice = ("scissors");
-                    break;
-            }
-            return choice;
-        }
-
-        public int SetWins()
-        {
-            int winTotal = wins++;
-            return winTotal;
-        }
-
-        public int GetWins()
-        {
-            return (wins);
         }
     }
 }
